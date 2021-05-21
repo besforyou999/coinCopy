1634cff131592ad2c646685c6b0fd0810c609b88
shfosem <37168972+shfosem@users.noreply.github.com>
2021-05-21 22:39:42 +0900

    Add files via upload

--- CoinCopy/CoinCopy/Chart.Designer.cs
+++ CoinCopy/CoinCopy/Chart.Designer.cs
@@ -29,57 +29,68 @@ namespace CoinCopy
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.priceChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.priceChart)).BeginInit();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // priceChart
-            // 
-            chartArea1.Name = "ChartArea1";
-            this.priceChart.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.priceChart.Legends.Add(legend1);
-            this.priceChart.Location = new System.Drawing.Point(12, 86);
-            this.priceChart.Name = "priceChart";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.priceChart.Series.Add(series1);
-            this.priceChart.Size = new System.Drawing.Size(533, 187);
-            this.priceChart.TabIndex = 0;
-            this.priceChart.Text = "chart1";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(26, 46);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.Size = new System.Drawing.Size(62, 15);
             this.label1.TabIndex = 1;
-            this.label1.Text = "종목명";
+            this.label1.Text = "종목명 :";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(118, 46);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(45, 15);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "label2";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(26, 84);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(82, 15);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "가격정보 : ";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(118, 84);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(45, 15);
+            this.lblPrice.TabIndex = 4;
+            this.lblPrice.Text = "label4";
             // 
             // Chart
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(575, 328);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lblName);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.priceChart);
             this.Name = "Chart";
             this.Text = "Chart";
-            ((System.ComponentModel.ISupportInitialize)(this.priceChart)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataVisualization.Charting.Chart priceChart;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl2;
+        internal System.Windows.Forms.Label lblName;
+        internal System.Windows.Forms.Label lblPrice;
     }
 }
